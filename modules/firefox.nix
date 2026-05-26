a15e1ec4b5b465e746c8c71cfee21563b1f4c17a
MilkyCode13 <shmayhel.andrey@gmail.com>
2026-05-26 23:07:39 +0300

    add firefox config persistence

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,10 +1,18 @@
+{ config, ... }:
 {
   programs.firefox = {
     enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     policies = {
       DisableTelemetry = true;
       SearchEngines.Default = "DuckDuckGo";
       SkipTermsOfUse = true;
     };
+  };
+
+  home.persistence."/persistent" = {
+    directories = [
+      ".config/mozilla"
+    ];
   };
 }
